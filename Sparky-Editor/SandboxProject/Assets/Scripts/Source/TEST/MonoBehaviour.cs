03fd4c0435c68d5e1015bc0a8ca2749cb73ee726
young_og <jessieshukeireh2001@gmail.com>
2022-11-27 15:32:39 -0500

    rebuild script core

--- Sparky-Editor/SandboxProject/Assets/Scripts/Source/TEST/MonoBehaviour.cs
+++ Sparky-Editor/SandboxProject/Assets/Scripts/Source/TEST/MonoBehaviour.cs
@@ -6,11 +6,19 @@ namespace Sandbox {
 	{
 		protected override void OnCreate()
 		{
-			
 		}
 
 		protected override void OnUpdate(float deltaTime)
 		{
+			if (Input.IsKeyDown(KeyCode.W))
+			{
+				transform.Translate(Vector3.Forward * deltaTime);
+			}
+
+			if (Input.IsMouseButtonDown(MouseButton.Left))
+			{
+
+			}
 		}
 	}
 
