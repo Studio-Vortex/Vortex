fe8cfff07dd79ed4c0ea74ae8b00484c3fb316da
young_og <jessieshukeireh2001@gmail.com>
2023-01-26 23:09:12 -0500

    removed unused libraries and moved stb_image back to its own directory
    - removed tinyobjloader and tinygltfloader as we have no use for them now
    - stb_image is back to it's own directory

--- premake5.lua
+++ premake5.lua
@@ -34,8 +34,6 @@ IncludeDir["msdfgen"] = "Vortex/vendor/msdf-atlas-gen/msdfgen"
 IncludeDir["PhysX"] = "Vortex/vendor/PhysX/include"
 IncludeDir["spdlog"] = "Vortex/vendor/spdlog/include"
 IncludeDir["stb_image"] = "Vortex/vendor/stb_image"
-IncludeDir["tinygltf"] = "Vortex/vendor/tinygltf"
-IncludeDir["tinyobjloader"] = "Vortex/vendor/tinyobjloader"
 IncludeDir["yaml_cpp"] = "Vortex/vendor/yaml-cpp/include"
 
 LibraryDir = {}
@@ -91,11 +89,11 @@ project "Vortex"
 		"%{prj.name}/src/**.cpp",
 		"%{prj.name}/vendor/glm/glm/**.hpp",
 		"%{prj.name}/vendor/glm/glm/**.inl",
+
+		"%{prj.name}/vendor/stb_image/**.h",
+		"%{prj.name}/vendor/stb_image/**.cpp",
+
 		"%{prj.name}/vendor/miniaudio/miniaudio/miniaudio.h",
-		"%{prj.name}/vendor/tinygltf/**.h",
-		"%{prj.name}/vendor/tinygltf/**.hpp",
-		"%{prj.name}/vendor/tinygltf/**.cpp",
-		"%{prj.name}/vendor/tinyobjloader/tiny_obj_loader.h",
 
 		"%{prj.name}/vendor/ImGuizmo/ImGuizmo.h",
 		"%{prj.name}/vendor/ImGuizmo/ImGuizmo.cpp",
@@ -131,8 +129,6 @@ project "Vortex"
 		"%{IncludeDir.PhysX}",
 		"%{IncludeDir.spdlog}",
 		"%{IncludeDir.stb_image}",
-		"%{IncludeDir.tinygltf}",
-		"%{IncludeDir.tinyobjloader}",
 		"%{IncludeDir.yaml_cpp}",
 	}
 
@@ -152,6 +148,7 @@ project "Vortex"
 		"%{Library.PhysXFoundation}",
 		"%{Library.PhysXPvd}",
 		"opengl32.lib",
+		"d3d11.lib",
 		"Ws2_32.lib",
 		"yaml-cpp",
 	}
@@ -256,8 +253,6 @@ project "Vortex-Editor"
 		"%{IncludeDir.PhysX}",
 		"%{IncludeDir.PhysX}/PhysX",
 		"%{IncludeDir.spdlog}",
-		"%{IncludeDir.tinygltf}",
-		"%{IncludeDir.tinyobjloader}",
 	}
 	
 	defines
@@ -455,8 +450,6 @@ project "Vortex-Runtime"
 		"%{IncludeDir.PhysX}",
 		"%{IncludeDir.PhysX}/PhysX",
 		"%{IncludeDir.spdlog}",
-		"%{IncludeDir.tinygltf}",
-		"%{IncludeDir.tinyobjloader}",
 	}
 
 	defines
--- scripts/Win32-GenProjects.bat
+++ scripts/Win32-GenProjects.bat
@@ -1,7 +1,6 @@
 @echo off
 
-pushd ../
+cd ..\
 call vendor\bin\premake\premake5.exe vs2022
-popd
 
 pause
