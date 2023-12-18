57f97f590390b715e2c37b70e99ba0486c886e94
young_og <jessieshukeireh2001@gmail.com>
2023-12-18 15:32:55 -0500

    drawVec3Controls refactor

--- Vortex/src/Vortex/Editor/UI/UI.h
+++ Vortex/src/Vortex/Editor/UI/UI.h
@@ -365,97 +365,6 @@ namespace Vortex::UI {
 		}
 	};
 
-	inline static void DrawVec3Controls(const std::string& label, Math::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f, std::function<void()> uiCallback = nullptr)
-	{
-		ImGuiIO& io = Gui::GetIO();
-		const auto& boldFont = io.Fonts->Fonts[0];
-
-		Gui::PushID(label.c_str());
-
-		Gui::Columns(2);
-		Gui::SetColumnWidth(0, columnWidth);
-		Gui::Text(label.c_str());
-		Gui::NextColumn();
-
-		Gui::PushMultiItemsWidths(3, Gui::CalcItemWidth());
-		Gui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
-
-		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
-		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
-
-		Gui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
-		Gui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.9f, 0.2f, 0.2f, 1.0f });
-		Gui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
-		Gui::PushFont(boldFont);
-		if (Gui::Button("X", buttonSize))
-		{
-			values.x = resetValue;
-
-			if (uiCallback != nullptr)
-			{
-				std::invoke(uiCallback);
-			}
-		}
-		Gui::PopFont();
-		Gui::PopStyleColor(3);
-
-		Gui::SameLine();
-		if (Gui::DragFloat("##X", &values.x, 0.1f, 0.0f, 0.0f, "%.2f") && uiCallback != nullptr)
-			uiCallback();
-		DrawItemActivityOutline();
-		Gui::PopItemWidth();
-		Gui::SameLine();
-
-		Gui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
-		Gui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.3f, 0.8f, 0.3f, 1.0f });
-		Gui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
-		Gui::PushFont(boldFont);
-		if (Gui::Button("Y", buttonSize))
-		{
-			values.y = resetValue;
-
-			if (uiCallback != nullptr)
-			{
-				std::invoke(uiCallback);
-			}
-		}
-		Gui::PopFont();
-		Gui::PopStyleColor(3);
-
-		Gui::SameLine();
-		if (Gui::DragFloat("##Y", &values.y, 0.1f, 0.0f, 0.0f, "%.2f") && uiCallback != nullptr)
-			uiCallback();
-		DrawItemActivityOutline();
-		Gui::PopItemWidth();
-		Gui::SameLine();
-
-		Gui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
-		Gui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.2f, 0.35f, 0.9f, 1.0f });
-		Gui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
-		Gui::PushFont(boldFont);
-		if (Gui::Button("Z", buttonSize))
-		{
-			values.z = resetValue;
-
-			if (uiCallback != nullptr)
-			{
-				std::invoke(uiCallback);
-			}
-		}
-		Gui::PopFont();
-		Gui::PopStyleColor(3);
-
-		Gui::SameLine();
-		if (Gui::DragFloat("##Z", &values.z, 0.1f, 0.0f, 0.0f, "%.2f") && uiCallback != nullptr)
-			uiCallback();
-		DrawItemActivityOutline();
-		Gui::PopItemWidth();
-
-		Gui::PopStyleVar();
-		Gui::Columns(1);
-		Gui::PopID();
-	}
-
 	inline static void PushID()
 	{
 		Gui::PushID(s_UIContextID++);
@@ -619,6 +528,79 @@ namespace Vortex::UI {
 		Gui::PopItemWidth();
 		Gui::NextColumn();
 		s_CheckboxCount = 0;
+	}
+
+	inline static void DrawVec3Controls(const std::string& label, Math::vec3& values, float resetValue = 0.0f, float columnWidth = 100.0f, std::function<void()> uiCallback = nullptr)
+	{
+		Gui::PushID(label.c_str());
+
+		Gui::Columns(2);
+		Gui::SetColumnWidth(0, columnWidth);
+		Gui::Text(label.c_str());
+		Gui::NextColumn();
+
+		Gui::PushMultiItemsWidths(3, Gui::CalcItemWidth());
+		Gui::PushStyleVar(ImGuiStyleVar_ItemSpacing, ImVec2{ 0, 0 });
+
+		float lineHeight = GImGui->Font->FontSize + GImGui->Style.FramePadding.y * 2.0f;
+		ImVec2 buttonSize = { lineHeight + 3.0f, lineHeight };
+
+		auto button = [&](auto label, auto index) {
+			PushFont("Bold");
+			if (Gui::Button(label, buttonSize))
+			{
+				values[index] = resetValue;
+
+				if (uiCallback != nullptr)
+				{
+					std::invoke(uiCallback);
+				}
+			}
+			PopFont();
+			Gui::PopStyleColor(3);
+		};
+
+		auto drag = [&](auto label, auto index) {
+			if (Gui::DragFloat(label, &values[index], 0.1f, -FLT_MAX, FLT_MAX, "%.2f"))
+			{
+				if (uiCallback != nullptr)
+				{
+					std::invoke(uiCallback);
+				}
+			}
+			DrawItemActivityOutline();
+			Gui::PopItemWidth();
+		};
+
+		Gui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
+		Gui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.9f, 0.2f, 0.2f, 1.0f });
+		Gui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.8f, 0.1f, 0.15f, 1.0f });
+		button("X", 0);
+		Gui::SameLine();
+
+		drag("##X", 0);
+		Gui::SameLine();
+
+		Gui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
+		Gui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.3f, 0.8f, 0.3f, 1.0f });
+		Gui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.2f, 0.7f, 0.2f, 1.0f });
+		button("Y", 1);
+		Gui::SameLine();
+
+		drag("##Y", 1);
+		Gui::SameLine();
+
+		Gui::PushStyleColor(ImGuiCol_Button, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
+		Gui::PushStyleColor(ImGuiCol_ButtonHovered, ImVec4{ 0.2f, 0.35f, 0.9f, 1.0f });
+		Gui::PushStyleColor(ImGuiCol_ButtonActive, ImVec4{ 0.1f, 0.25f, 0.8f, 1.0f });
+		button("Z", 2);
+		Gui::SameLine();
+
+		drag("##Z", 2);
+
+		Gui::PopStyleVar();
+		Gui::Columns(1);
+		Gui::PopID();
 	}
 
 	inline static bool PropertyCheckboxGroup(const char* label, bool& value)
